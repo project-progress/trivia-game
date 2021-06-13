10a37c22e3e08860ed5ca5024c5145ccea43f5f6
Davit1996 <forschools1996@gmail.com>
2021-06-13 14:20:01 +0400

    Test

--- JS/GamePage.js
+++ JS/GamePage.js
@@ -1,7 +1,7 @@
 class User{
-  constructor(n){
+  constructor(n,m){
     this.name = n;
-    this.scores = 0;
+    this.scores = m;
   }
   addScore(score){
     this.scores += score;
@@ -18,12 +18,12 @@ if(window.localStorage.length){
     let value = window.localStorage.getItem(user);
     //check for value, because for loop get also methods of local storage
     if(value){
-      users.push(new User(value));
+      users.push(new User(JSON.parse(value).name,JSON.parse(value).scores));
       let usersContainer = document.getElementById("usersContainer");
       let usersHTML = "";
       users.forEach(user => {
         usersHTML += `<div class="mb-3">
-            <label>${user.name} - ${user.scores}</label>
+            <label>${user.name} -${user.scores}</label>
             <input type="text" id="${user.name}"></input>
         </div>`;
       });
@@ -43,11 +43,61 @@ function categories(){
       resp.json().then(function (data){
         
           for(let i=1;i<=5;i++){
-            let info=data[Math.round(Math.random()*10)].title;
-            document.getElementById(`r${i}`).nodeValue=info;
-            document.getElementById(`l${i}`).innerHTML= info;
+            let question=data[Math.round((Math.random()*20)+Math.random()*10)];
+            let info=question.title;
+            let id=question.id;
+            document.getElementById(`r${i}`).value=id;
+            document.getElementById(`l${i}`).innerHTML= `${info}`;
           }
       })
    })
    
+}
+//load the question
+function bringquestion(){
+  try {
+    let category=document.querySelector("input[name='question']:checked").value;
+    let value=document.querySelector("input[name='exampleRadios']:checked").value;
+    console.log(category);
+    console.log(value);
+    fetch(`https://jservice.io/api/clues?category=${category}&value=${value}`).then(function(response){
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+      response.json().then(function(data){
+       if(data.length>0){
+        document.getElementById("questionarea").style.padding="10px";
+        document.getElementById("answerarea").style.padding="10px";
+        document.getElementById("questionarea").innerHTML=data[0].question;
+        document.getElementById("answerarea").innerHTML=`The answer is ${data[0].answer}`;
+        console.log(data);
+       }
+        else {
+          alert("change the vlaue");
+        }
+      })
+    })
+  }  
+  catch {
+    alert("the category or value was not choosen");
+  }
+ 
+    
+}
+//adding of question values to every player
+function addResult()
+{
+   for(let i=1;i<users.length+1;i++){
+     let user=JSON.parse(window.localStorage.getItem(`User${i}`));
+     if(document.getElementById(`${user.name}`).value==""){
+      user.scores+=0;
+     }
+     else{
+      user.scores+=parseInt(document.getElementById(`${user.name}`).value);;
+     }
+     window.localStorage.setItem(`User${i}`,JSON.stringify(user));
+   }
+   location.reload();
 }
--- JS/Project.js
+++ JS/Project.js
@@ -1,3 +1,16 @@
+class User{
+    constructor(n,m){
+      this.name = n;
+      this.scores = m;
+    }
+    addScore(score){
+      this.scores += score;
+    }
+    removeScore(score){
+      this.scores -= score;
+    }
+  }
+  
 //If Users aare in the local storage - load them.
 if(window.localStorage.length){
     for(user in window.localStorage){
@@ -5,6 +18,7 @@ if(window.localStorage.length){
         //check for value, because for loop get also methods of local storage
         if(value){
             let el = document.getElementById(user);
+            console.log(el)
             el.querySelector("input[type=checkbox]").checked = true;
             let textBox = el.querySelector("input[type=text]");
             textBox.disabled = null;
@@ -26,14 +40,15 @@ function button()
     let checkboxarray=document.querySelectorAll("input[type=checkbox]");
     let indicator=0;
     for(let i=0;i<checkboxarray.length;i++){
-    if(checkboxarray[i].checked){
+    if(checkboxarray[i].checked && document.getElementById(`input${i+1}`).value!==""){
         indicator++;
     }
    }
    if(indicator>=2){
-    for(let i=1;i<=indicator;i++){
-        localStorage.setItem(`User${i}`,document.getElementById(`input${i}`).value);
-    }     
+ for(let i=1;i<=indicator;i++){
+        let user = new User(document.getElementById(`input${i}`).value,0);
+        localStorage.setItem(`User${i}`,JSON.stringify(user));
+    } 
     window.location='./GamePage.html';
    }
    else {
